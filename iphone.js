bad8d75601c8913d01829c1d51b909e5bc0add60
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-26 14:41:10 +0200

    first commit

---
+++ iphone.js
@@ -0,0 +1,37 @@
+let image = document.querySelector("#iphone")
+let iphone1 = document.querySelector("#ipone1")
+let iphone2 = document.querySelector("#ipone2")
+let iphone3 = document.querySelector("#ipone3")
+let iphone4 = document.querySelector("#ipone4")
+let iphone5 = document.querySelector("#ipone5")
+
+
+
+function ap1(){
+    document.body.style.background = "#FFA500";
+    image.src="abdelrahman gamal/0.png"
+}
+
+function ap2(){
+    document.body.style.background = "#ADD8E6";
+    image.src="abdelrahman gamal/1.png"
+    
+}
+
+function ap3(){
+    document.body.style.background = "#808080";
+    image.src="abdelrahman gamal/2.png"
+    
+}
+
+function ap4(){
+    document.body.style.background = "wheat";
+    image.src="abdelrahman gamal/3.png"
+}
+
+function ap5(){
+    document.body.style.background = "red";
+    image.src="abdelrahman gamal/4.png"
+}
+
+
